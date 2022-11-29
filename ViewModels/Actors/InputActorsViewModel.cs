7d6e225cf5b1a89ae1d06ef8d6f5a63962f336df
Nikita <nikson6911@mail.ru>
2022-11-29 14:49:44 +0300

    Commit

---
+++ ViewModels/Actors/InputActorsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MoviesApp.ViewModels.Actors;
+
+public class InputActorsModel
+{
+    public string Name { get; set; }
+        
+    public string Surname { get; set; }
+
+    [DataType(DataType.Date)]
+    public DateTime DateOfBirth { get; set; }
+}
