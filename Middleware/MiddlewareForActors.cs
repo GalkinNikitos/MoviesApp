0b2de67a9bbbce1371a9df68e1bdc28ca0f38d0a
Nikita <nikson6911@mail.ru>
2022-12-05 22:36:59 +0300

    Task2_MiddleWare

---
+++ Middleware/MiddlewareForActors.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.Extensions.Logging;
+
+namespace MoviesApp.Middleware;
+
+public class MiddlewareForActors
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<MiddlewareForActors> _logger;
+
+    public MiddlewareForActors(RequestDelegate next, ILogger<MiddlewareForActors> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public  Task InvokeAsync(HttpContext httpContext)
+    {
+        if (httpContext.Request.GetDisplayUrl().Contains("Actor"))
+        {
+            _logger.LogInformation(httpContext.Request.Method);
+            _logger.LogInformation(httpContext.Request.Path);
+            _logger.LogInformation(httpContext.Request.Protocol);
+            _logger.LogInformation(httpContext.Request.ContentType);
+            _logger.LogInformation(httpContext.Request.QueryString.Value);
+        }
+
+        return _next(httpContext);
+    }
+}
+
+public static class CustomMiddlewareForActorsExtension
+{
+    public static IApplicationBuilder UseCustom(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<MiddlewareForActors>();
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using MoviesApp.Data;
+using MoviesApp.Middleware;
 
 namespace MoviesApp
 {
@@ -37,10 +38,13 @@ namespace MoviesApp
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCustom();
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
